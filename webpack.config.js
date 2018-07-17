3a884204ad52bafd8882a7047bd9356d763b82a4
Alexandra Ash <alexandraleah@gmail.com>
2018-07-17 15:09:35 -0400

    set up simple server and webpack confit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
+  //useing the default entry src/index.js
   mode: 'development',
   output: {
     path: __dirname, // assumes your bundle.js will also be in the root of your project folder
-    filename: 'bundle.js',
+    filename: './public/bundle.js',
   },
   devtool: 'source-maps',
   module: {
